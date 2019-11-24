b150c5433b58ab9938b332a6c5ab6f30498aeba4
Deon Choi <deon.choi@gmail.com>
2019-11-23 20:17:05 -0800

    Updates

---
+++ precache-manifest.b43db48e3173a106fd2a9c536cac0c6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9acd8dd26d368774124eb51103c133e",
+    "url": "/flix-app/index.html"
+  },
+  {
+    "revision": "5c1c0720b5c2ea2cba38",
+    "url": "/flix-app/static/css/main.e7ed229e.chunk.css"
+  },
+  {
+    "revision": "bbeccb151f85c6d27910",
+    "url": "/flix-app/static/js/2.6c7b2984.chunk.js"
+  },
+  {
+    "revision": "5c1c0720b5c2ea2cba38",
+    "url": "/flix-app/static/js/main.0376a140.chunk.js"
+  },
+  {
+    "revision": "da21b0724d55f52dc165",
+    "url": "/flix-app/static/js/runtime-main.26b9e7c8.js"
+  },
+  {
+    "revision": "53a046a1880602a10495074247fcc4b4",
+    "url": "/flix-app/static/media/background.53a046a1.jpg"
+  },
+  {
+    "revision": "cab44ad951b98feac83f7e49647589a7",
+    "url": "/flix-app/static/media/search.cab44ad9.png"
+  },
+  {
+    "revision": "e618b36bb8e47fed3b355e035074c4ef",
+    "url": "/flix-app/static/media/siteLogo.e618b36b.jpg"
+  }
+]);
